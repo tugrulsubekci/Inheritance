82be05eb7e6a0087cb0b37461db12237d86f1f61
Tuğrul Sübekci <tugrulsubekci@gmail.com>
2022-05-27 11:21:27 +0300

    Add project files.

---
+++ Inheritance/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Sedan s = new Sedan(60);
+            // Call SpeedUp() here
+            Console.WriteLine(s.Describe());
+
+            Truck t = new Truck(45, 500);
+            // Call SpeedUp() here
+            Console.WriteLine(t.Describe());
+
+            Bicycle b = new Bicycle(10);
+            // Call SpeedUp() here
+            Console.WriteLine(b.Describe());
+
+            //to keep console open
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Inheritance/Vehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    abstract class Vehicle
+    {
+        public string LicensePlate
+        { get; private set; }
+
+        public double Speed
+        { get; protected set; }
+
+        public int Wheels
+        { get; protected set; }
+
+        public Vehicle(double speed)
+        {
+            Speed = speed;
+            LicensePlate = Tools.GenerateLicensePlate();
+        }
+
+        public virtual void SpeedUp()
+        {
+            Speed += 5;
+        }
+
+        public virtual void SlowDown()
+        {
+            Speed -= 5;
+        }
+
+        public void Honk()
+        {
+            Console.WriteLine("HONK!");
+        }
+
+        public abstract string Describe();
+    }
+}
